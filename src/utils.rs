85028a77ec21a8bdde87d0cfa9879a3616dcbcf1
ebukapeta <ucpeter10@yahoo.com>
2025-08-24 12:23:38 +0100

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,16 @@
-pub fn percent_string(p: f64) -> String {
-    format!("{:.3}%", p * 100.0)
+use crate::models::TriangularResult;
+
+const FEE_PER_LEG: f64 = 0.1;
+const TOTAL_LEGS: f64 = 3.0;
+
+pub fn calculate_with_fees(triangle: String, raw_profit: f64) -> TriangularResult {
+    let total_fee = FEE_PER_LEG * TOTAL_LEGS;
+    let final_profit = raw_profit - total_fee;
+
+    TriangularResult {
+        triangle,
+        profit_before_fees: raw_profit,
+        trade_fees: total_fee,
+        profit_after_fees: final_profit,
+    }
 }
