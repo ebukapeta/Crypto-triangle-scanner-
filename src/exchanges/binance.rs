887f64e2e0acb8e47cbf4bad24e3aba2c0c41e72
ebukapeta <ucpeter10@yahoo.com>
2025-08-24 09:40:54 +0100

    Update binance.rs

--- src/exchanges/binance.rs
+++ src/exchanges/binance.rs
@@ -1,27 +1,24 @@
 use reqwest;
 use serde_json::Value;
 
-pub async fn fetch_prices() -> Vec<(String, String, f64)> {
+pub async fn fetch_prices() -> Result<Vec<(String, String, f64)>, reqwest::Error> {
     let mut out = Vec::new();
-    if let Ok(r) = reqwest::get("https://api.binance.com/api/v3/ticker/price")
-        .await
-        .and_then(|r| r.json::<Vec<Value>>())
-        .await
-    {
-        for item in r {
-            if let (Some(sym), Some(price_str)) = (item["symbol"].as_str(), item["price"].as_str())
-            {
-                if let Ok(price) = price_str.parse::<f64>() {
-                    // Basic split: last 4 chars as quote
-                    let (base, quote) = if sym.len() > 4 {
-                        (&sym[..sym.len() - 4], &sym[sym.len() - 4..])
-                    } else {
-                        (sym, "")
-                    };
-                    out.push((base.to_string(), quote.to_string(), price));
-                }
+
+    let resp = reqwest::get("https://api.binance.com/api/v3/ticker/price").await?;
+    let data = resp.json::<Vec<Value>>().await?;
+
+    for item in data {
+        if let (Some(sym), Some(price_str)) = (item["symbol"].as_str(), item["price"].as_str()) {
+            if let Ok(price) = price_str.parse::<f64>() {
+                let (base, quote) = if sym.len() > 4 {
+                    (&sym[..sym.len() - 4], &sym[sym.len() - 4..])
+                } else {
+                    (sym, "")
+                };
+                out.push((base.to_string(), quote.to_string(), price));
             }
         }
     }
-    out
-}
+
+    Ok(out)
+                     }
