f09810f844639fcfbf4beef2275181921ced0ed5
ebukapeta <ucpeter10@yahoo.com>
2025-08-24 12:29:08 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
-mod routes;
-mod utils;
-mod exchanges;
-
-use axum::{Router, Server};
-use std::net::SocketAddr;
+use axum::{Router};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use tower_http::cors::CorsLayer;
 use tower_http::services::ServeDir;
-use routes::create_router;
+
+mod models;
+mod utils;
+mod routes;
+mod exchanges;
 
 #[tokio::main]
 async fn main() {
-    let app = create_router().nest_service("/static", ServeDir::new("static"));
+    let shared_state = Arc::new(Mutex::new(Vec::new()));
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-    println!("Server running at http://{}", addr);
+    let app = Router::new()
+        .merge(routes::binance::binance_routes(shared_state.clone()))
+        .merge(routes::bybit::bybit_routes(shared_state.clone()))
+        .merge(routes::kucoin::kucoin_routes(shared_state.clone()))
+        .merge(routes::gateio::gateio_routes(shared_state.clone()))
+        .merge(routes::kraken::kraken_routes(shared_state.clone()))
+        .nest_service("/", ServeDir::new("static"))
+        .layer(CorsLayer::permissive());
 
-    Server::bind(&addr)
+    println!("Server running at http://127.0.0.1:3000");
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
-               }
+            }
