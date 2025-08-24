44d0e535c500e6b3578f618371a57e28117c792c
ebukapeta <ucpeter10@yahoo.com>
2025-08-24 22:08:47 +0100

    Update bybit.rs

--- src/exchanges/bybit.rs
+++ src/exchanges/bybit.rs
@@ -1,16 +1,15 @@
-use reqwest::Error;
-use serde_json::Value;
 use crate::models::PairPrice;
 use crate::utils::split_concat_symbol;
+use reqwest::Error;
+use serde_json::Value;
 
 pub async fn fetch_prices() -> Result<Vec<PairPrice>, Error> {
     let url = "https://api.bybit.com/v5/market/tickers?category=spot";
     let v: Value = reqwest::get(url).await?.json().await?;
     let mut out = Vec::new();
-
-    if let Some(list) = v["result"]["list"].as_array() {
+    if let Some(list) = v.get("result").and_then(|r| r.get("list")).and_then(|l| l.as_array()) {
         for item in list {
-            if let (Some(sym), Some(pstr)) = (item["symbol"].as_str(), item["lastPrice"].as_str()) {
+            if let (Some(sym), Some(pstr)) = (item.get("symbol").and_then(|v| v.as_str()), item.get("lastPrice").and_then(|v| v.as_str())) {
                 if let Ok(price) = pstr.parse::<f64>() {
                     if let Some((base, quote)) = split_concat_symbol(sym) {
                         out.push(PairPrice { base, quote, price });
@@ -19,6 +18,5 @@ pub async fn fetch_prices() -> Result<Vec<PairPrice>, Error> {
             }
         }
     }
-
     Ok(out)
 }
