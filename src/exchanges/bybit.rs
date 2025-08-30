9bf63fef0b177d0e63bc3f80f85f387b99a1dc2e
ebukapeta <ucpeter10@yahoo.com>
2025-08-30 12:19:10 +0100

    Update bybit.rs

--- src/exchanges/bybit.rs
+++ src/exchanges/bybit.rs
@@ -8,19 +8,18 @@ pub async fn fetch_prices() -> Result<Vec<PairPrice>, Error> {
     let resp: Value = reqwest::get(url).await?.json().await?;
     let mut out = Vec::new();
 
-    if let Some(tickers) = resp["result"]["list"].as_array() {
-        for item in tickers {
-            if let (Some(sym), Some(pstr)) =
-                (item.get("symbol").and_then(|v| v.as_str()), item.get("lastPrice").and_then(|v| v.as_str()))
-            {
-                if let Ok(price) = pstr.parse::<f64>() {
-                    if let Some((base, quote)) = split_concat_symbol(sym) {
+    if let Some(list) = resp["result"]["list"].as_array() {
+        for item in list {
+            let sym = item.get("symbol").and_then(|v| v.as_str());
+            let pstr = item.get("lastPrice").and_then(|v| v.as_str());
+            if let (Some(s), Some(ps)) = (sym, pstr) {
+                if let Ok(price) = ps.parse::<f64>() {
+                    if let Some((base, quote)) = split_concat_symbol(s) {
                         out.push(PairPrice { base, quote, price });
                     }
                 }
             }
         }
     }
-
     Ok(out)
-                    }
+}
