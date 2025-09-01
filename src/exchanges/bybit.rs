02a958d58d52d3fe97010c20b99b2d4426c5513a
ebukapeta <ucpeter10@yahoo.com>
2025-09-01 22:12:53 +0100

    Update bybit.rs

--- src/exchanges/bybit.rs
+++ src/exchanges/bybit.rs
@@ -5,16 +5,13 @@ use serde_json::Value;
 
 pub async fn fetch_prices() -> Result<Vec<PairPrice>, Error> {
     let url = "https://api.bybit.com/v5/market/tickers?category=spot";
-    let resp: Value = reqwest::get(url).await?.json().await?;
+    let v: Value = reqwest::get(url).await?.json().await?;
     let mut out = Vec::new();
-
-    if let Some(list) = resp["result"]["list"].as_array() {
+    if let Some(list) = v.get("result").and_then(|r| r.get("list")).and_then(|l| l.as_array()) {
         for item in list {
-            let sym = item.get("symbol").and_then(|v| v.as_str());
-            let pstr = item.get("lastPrice").and_then(|v| v.as_str());
-            if let (Some(s), Some(ps)) = (sym, pstr) {
-                if let Ok(price) = ps.parse::<f64>() {
-                    if let Some((base, quote)) = split_concat_symbol(s) {
+            if let (Some(sym), Some(pstr)) = (item.get("symbol").and_then(|s| s.as_str()), item.get("lastPrice").and_then(|s| s.as_str())) {
+                if let Ok(price) = pstr.parse::<f64>() {
+                    if let Some((base, quote)) = split_concat_symbol(sym) {
                         out.push(PairPrice { base, quote, price });
                     }
                 }
